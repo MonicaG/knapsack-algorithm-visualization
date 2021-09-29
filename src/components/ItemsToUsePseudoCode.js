c89f57b11b123f89a859c9615a4b8e84484131ba
Monica Granbois <mgranbois@outlook.com>
2021-09-29 14:17:13 -0700

    refactored to use the useReducer hook. Cleans up multiple useState hooks

--- src/components/ItemsToUsePseudoCode.js
+++ src/components/ItemsToUsePseudoCode.js
@@ -1,5 +1,9 @@
 function ItemsToUsePseudoCode({ item, capacity, index, solutionTable }) {
-  
+  return (
+    <div>
+      <p>hello world!</p>
+    </div>
+  )
 }
 
 export default ItemsToUsePseudoCode;
--- src/components/SolutionTable.js
+++ src/components/SolutionTable.js
@@ -1,25 +1,50 @@
 import PropTypes from 'prop-types';
 import SolutionTableRow from './SolutionTableRow';
 import TablePopulationPseudoCode from './TablePopulationPseudoCode';
-import React from 'react';
+import React, { useReducer } from 'react';
+import ItemsToUsePseudoCode from './ItemsToUsePseudoCode';
 
+
+const types = {
+  STEP_TO_NEXT_CELL: 1,
+  STEP_TO_NEXT_ROW: 2,
+}
+
+const initialState = {
+  currentItemIndex: 1,
+  currentCapacity: 1,
+  currentCellIndex: 1,
+}
 
 function SolutionTable({ knapsackAlgorithm }) {
-  const [currentItemIndex, setCurrentItemIndex] = React.useState(1);
-  const [currentCapacity, setCurrentCapacity] = React.useState(1);
-  const [currentCellIndex, setCurrentCellIndex] = React.useState(1);
+  const [state, dispatch] = useReducer(reducer, initialState);
   const capacityRow = Array.from({ length: knapsackAlgorithm.capacity + 1 }, (e, i) => i);
 
-  function btnClick() {
-      if (currentCapacity === knapsackAlgorithm.capacity) {
-        setCurrentCapacity(1);
-        setCurrentItemIndex(currentItemIndex + 1);
-        setCurrentCellIndex(1);
-      } else {
-        setCurrentCapacity(currentCapacity + 1);
-        setCurrentCellIndex(currentCellIndex+1);
-      }
+  function getType() {
+    return state.currentCapacity === knapsackAlgorithm.capacity ? types.STEP_TO_NEXT_ROW : types.STEP_TO_NEXT_CELL
   }
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case types.STEP_TO_NEXT_CELL :
+        return { 
+          ...state, 
+          currentCapacity: state.currentCapacity + 1,
+          currentCellIndex: state.currentCellIndex + 1
+        }
+      case types.STEP_TO_NEXT_ROW :
+        return { 
+          ...state, 
+          currentItemIndex: state.currentItemIndex + 1,
+          currentCapacity: 1,
+          currentCellIndex: 1
+        };
+      default: 
+        //@todo should default do something else?
+        throw new Error();
+    }
+  }
+
   return (
     <div className="SolutionTable">
       <table border="1">
@@ -32,9 +57,9 @@ function SolutionTable({ knapsackAlgorithm }) {
           {knapsackAlgorithm.solutionTable.map((row, index) => {
             const indexOffset = index - 1;
             const item = index === 0 ? null : knapsackAlgorithm.items[indexOffset]
-            const highlightCellIndex = index === currentItemIndex ? currentCellIndex : null;
+            const highlightCellIndex = index === state.currentItemIndex ? state.currentCellIndex : null;
             const formattedRow = row.map((element, rowIndex) => {
-              return index < currentItemIndex || (index === currentItemIndex && rowIndex <= currentCapacity) ? element : 0;
+              return index < state.currentItemIndex || (index === state.currentItemIndex && rowIndex <= state.currentCapacity) ? element : 0;
             });
             return <SolutionTableRow
               key={item ? item.name : " "}
@@ -46,17 +71,17 @@ function SolutionTable({ knapsackAlgorithm }) {
           })}
         </tbody>
       </table>
-      <input type="button" value="Step" onClick={btnClick} />
+      <input type="button" value="Step" onClick={ () => dispatch({type: getType()}) } />
       <div>
-        <p>current capacity is: {currentCapacity}</p>
-        <p>current item Index is: {currentItemIndex}</p>
-        {currentItemIndex > knapsackAlgorithm.items.length ?
-        null
+        <p>current capacity is: {state.currentCapacity}</p>
+        <p>current item Index is: {state.currentItemIndex}</p>
+        {state.currentItemIndex > knapsackAlgorithm.items.length ?
+        <ItemsToUsePseudoCode/>
         :
         <TablePopulationPseudoCode
-            item={knapsackAlgorithm.items[currentItemIndex - 1]}
-            capacity={currentCapacity}
-            index={currentItemIndex}
+            item={knapsackAlgorithm.items[state.currentItemIndex - 1]}
+            capacity={state.currentCapacity}
+            index={state.currentItemIndex}
             solutionTable={knapsackAlgorithm.solutionTable}
           />
         }
