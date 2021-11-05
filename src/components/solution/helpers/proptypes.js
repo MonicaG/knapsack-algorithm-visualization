eefe0f5b416d6ccfdabfb45ce824a2f169fbeb0c
Monica Granbois <mgranbois@outlook.com>
2021-11-05 14:09:36 -0700

    reformatted file

--- src/components/solution/helpers/proptypes.js
+++ src/components/solution/helpers/proptypes.js
@@ -1,20 +1,20 @@
 import PropTypes from 'prop-types';
 
-export const ItemPropType =  PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    value: PropTypes.number.isRequired,
-    weight: PropTypes.number.isRequired
-  });
+export const ItemPropType = PropTypes.shape({
+  name: PropTypes.string.isRequired,
+  value: PropTypes.number.isRequired,
+  weight: PropTypes.number.isRequired
+});
 
 export const KnapsackAlgorithmPropType = PropTypes.shape({
-    items: PropTypes.arrayOf(ItemPropType).isRequired,
-    capacity: PropTypes.number.isRequired,
-    solutionTable: PropTypes.array.isRequired
-  });
+  items: PropTypes.arrayOf(ItemPropType).isRequired,
+  capacity: PropTypes.number.isRequired,
+  solutionTable: PropTypes.array.isRequired
+});
 
 export const SolutionItemsPropType = PropTypes.arrayOf(
-    PropTypes.shape({
-      item: ItemPropType.isRequired,
-      row: PropTypes.number.isRequired,
-      column: PropTypes.number.isRequired
-    }));
+  PropTypes.shape({
+    item: ItemPropType.isRequired,
+    row: PropTypes.number.isRequired,
+    column: PropTypes.number.isRequired
+  }));
